b686378c1e221694e06808451c4756d62aa0d126
gennaroderosa10 <gennaroderosa5@gmail.com>
2026-01-08 16:19:48 +0100

    feat: error done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import postsRouters from "./routers/posts.js"
 import pageNotFound from "./middlewares/pageNotFound.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 const port = 3020;
@@ -19,6 +20,8 @@ app.get("/", (req, res) => {
 app.use("/posts", postsRouters)
 
 app.use(pageNotFound);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`il sever è in ascolto sulla porta ${port}`);
